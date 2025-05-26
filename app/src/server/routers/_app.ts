27ad3faecc339a2967d9bf314d73eff8da203861
Danzel Tan <47131877+shagrirse@users.noreply.github.com>
2025-05-26 22:53:22 +0800

    chore(starter-kit): update deprecated subscription (#3)

--- app/src/server/routers/_app.ts
+++ app/src/server/routers/_app.ts
@@ -3,23 +3,17 @@
  */
 import { router, publicProcedure } from '../trpc'
 import { postRouter } from './post'
-import { observable } from '@trpc/server/observable'
-import { clearInterval } from 'timers'
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
 
   post: postRouter,
 
-  randomNumber: publicProcedure.subscription(() => {
-    return observable<number>((emit) => {
-      const int = setInterval(() => {
-        emit.next(Math.random())
-      }, 500)
-      return () => {
-        clearInterval(int)
-      }
-    })
+  randomNumber: publicProcedure.subscription(async function* () {
+    while (true) {
+      yield Math.random()
+      await new Promise((resolve) => setTimeout(resolve, 500))
+    }
   }),
 })
 
