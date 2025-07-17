0e0a96041a213ae2bae14e9a76c4657903e62654
shagrirse <shagrirse@gmail.com>
2025-07-18 00:14:03 +0800

    wip: telegram start command test

---
+++ app/src/services/telegram-bot/features/start/start.test.ts
@@ -0,0 +1,10 @@
+import '../../test-utils'
+import { prismaMock } from '~/../test/prisma-mock'
+
+describe('start command (onboarding)', () => {
+  it.todo('user found and displayed feature menu')
+
+  it.todo('invalid onboarding token')
+
+  it.todo('valid onboarding token')
+})
